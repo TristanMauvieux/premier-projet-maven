d0500bff5f7fad83a7fe23ba195ff9ce8942031e
Tristan Mauvieux <mauvieux.tristan@gmail.com>
2018-04-16 15:22:01 +0200

    fin du TP3, configuration du pom.XML pour definir les profils DEV et Prod

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 import com.github.lalyos.jfiglet.FigletFont;
+import java.util.ResourceBundle;
 
 /**
  * Hello world!
@@ -9,9 +10,12 @@ public class App
 {
     public static void main( String[] args )
     {
-    	 String asciiArt = FigletFont.convertOneLine("hello");
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	 String asciiArt = FigletFont.convertOneLine(titre);
     	    System.out.println(asciiArt);
-		
+        	String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        	System.out.println("Environnement : "+ environnement);
+
         
     }
     
