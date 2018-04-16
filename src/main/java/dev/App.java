a8554bce8f30bb14605ba0e2be078ca640a646da
Tristan Mauvieux <mauvieux.tristan@gmail.com>
2018-04-16 11:27:05 +0200

    test des commandes maven niveau compilation de fichier

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
+    	String message = "coucou je suis un message";
         System.out.println( "Hello World!" );
+		
+        
     }
+    
 }
